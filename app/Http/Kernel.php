99c6265c1b6af56bc0c8a4901fa02eac720d3e8f
vagrant <vagrant@homestead>
2018-01-12 08:17:29 +0000

    change CORS middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
@@ -58,7 +59,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'self' => RejectIfNotSelf::class,
-        'cors' => \App\Http\Middleware\Cors::class,
+        'self' => RejectIfNotSelf::class
     ];
 }
